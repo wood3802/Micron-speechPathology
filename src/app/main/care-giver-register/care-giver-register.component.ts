f3b5febf4b0f5131ab9b319cf12e3eb008a0e059
Silence_JL <ma6724@vandals.uidaho.edu>
2020-04-01 11:47:31 -0700

    register update

--- src/app/main/care-giver-register/care-giver-register.component.ts
+++ src/app/main/care-giver-register/care-giver-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-care-giver-register',
   templateUrl: './care-giver-register.component.html',
@@ -12,4 +12,8 @@ export class CareGiverRegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
 }
