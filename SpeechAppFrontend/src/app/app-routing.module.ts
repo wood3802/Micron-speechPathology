d7ce5b5c4b01aed173ff26b75b2a9484d14aa26d
wood3802 <40341288+wood3802@users.noreply.github.com>
2020-05-01 00:36:35 -0600

    Update app-routing.module.ts

--- SpeechAppFrontend/src/app/app-routing.module.ts
+++ SpeechAppFrontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PatientsComponent } from './patients/patients.component';
 import { AuthGuardService } from './main/services/auth-guard.service';
+import {UploadPageComponent} from './upload-page/upload-page.component';
 
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuardService]},
-  { path: 'patient', component: PatientsComponent, canActivate:[AuthGuardService]}
+  { path: 'patient', component: PatientsComponent, canActivate:[AuthGuardService]},
+  { path: 'upload', component: UploadPageComponent}
   ];
 
 @NgModule({
