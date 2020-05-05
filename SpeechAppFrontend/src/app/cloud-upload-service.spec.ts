9066a2f76fbe3ab200b02951bc4594567dd4e2ed
wood3802 <40341288+wood3802@users.noreply.github.com>
2020-05-04 21:47:58 -0600

    Create cloud-upload-service.spec.ts

---
+++ SpeechAppFrontend/src/app/cloud-upload-service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CloudUploadService } from './cloud-upload.service';
+
+describe('CloudUploadService', () => {
+  let service: CloudUploadService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CloudUploadService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
